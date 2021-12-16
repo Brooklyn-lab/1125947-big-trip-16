95edf51a2938d1506b0722f74eed728cca1dfc2a
Brooklyn-lab <stepanyukilja@gmail.com>
2021-12-16 13:04:19 +0200

    add new fix

--- src/view/main-trip-point-view.js
+++ src/view/main-trip-point-view.js
@@ -2,69 +2,69 @@ import dayjs from 'dayjs';
 import { createElement } from '../render.js';
 
 const createMainTripPointTemplate = (point) => {
-   const { basePrice, dateFrom, dateTo, destination: { name }, offer: { type } } = point;
-   const generateDate = (date) => dayjs(date).format('h:mm:ss A');
+  const { basePrice, dateFrom, dateTo, destination: { name }, offer: { type } } = point;
+  const generateDate = (date) => dayjs(date).format('h:mm:ss A');
 
-   return `<li class="trip-events__item">
+  return `<li class="trip-events__item">
       <div class="event">
       <time class="event__date" datetime="2019-03-18">MAR 18</time>
       <div class="event__type">
-         <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${name}</h3>
       <div class="event__schedule">
-         <p class="event__time">
+        <p class="event__time">
             <time class="event__start-time" datetime="2019-03-18T10:30">${generateDate(dateFrom)}</time>
             &mdash;
             <time class="event__end-time" datetime="2019-03-18T11:00">${generateDate(dateTo)}</time>
-         </p>
-         <p class="event__duration">30M</p>
+        </p>
+        <p class="event__duration">30M</p>
       </div>
       <p class="event__price">
-         &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
+        &euro;&nbsp;<span class="event__price-value">${basePrice}</span>
       </p>
       <h4 class="visually-hidden">Offers:</h4>
       <ul class="event__selected-offers">
-         <li class="event__offer">
+        <li class="event__offer">
             <span class="event__offer-title">Order Uber</span>
             &plus;&euro;&nbsp;
             <span class="event__offer-price">20</span>
-         </li>
+        </li>
       </ul>
       <button class="event__favorite-btn event__favorite-btn--active" type="button">
-         <span class="visually-hidden">Add to favorite</span>
-         <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+        <span class="visually-hidden">Add to favorite</span>
+        <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
-         </svg>
+        </svg>
       </button>
       <button class="event__rollup-btn" type="button">
-         <span class="visually-hidden">Open event</span>
+        <span class="visually-hidden">Open event</span>
       </button>
       </div>
-   </li>`
+  </li>`;
 };
 
 export default class MainTripPointView {
-   #element = null;
-   #point = null;
+  #element = null;
+  #point = null;
 
-   constructor(point) {
-      this.#point = point
-   }
+  constructor(point) {
+    this.#point = point;
+  }
 
-   get element() {
-      if (!this.#element) {
-         this.#element = createElement(this.template);
-      }
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
 
-      return this.#element;
-   }
+    return this.#element;
+  }
 
-   get template() {
-      return createMainTripPointTemplate(this.#point);
-   }
+  get template() {
+    return createMainTripPointTemplate(this.#point);
+  }
 
-   removeElement() {
-      this.#element = null;
-   }
+  removeElement() {
+    this.#element = null;
+  }
 }
